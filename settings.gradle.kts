a5075a6eba0c2ba1dc5c8c1f0eacb1b57ee39434
Yrjan Fraschetti <yrjan@fraschetti.no>
2024-09-10 13:34:16 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "dp-SERVICENAME"
+dependencyResolutionManagement {
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.dagpenger:dp-version-catalog:20240814.85.8cafcc")
+        }
+    }
+}
