00d7b3ad43f200d22546e3e6948891b335ba0f15
abdifatah-bashi <abdifatah.bashi@nav.no>
2024-09-23 12:49:28 +0200

    Refaktorer PostgresPersonRepository

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/db/PostgresPersonRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/db/PostgresPersonRepository.kt
@@ -1,5 +1,6 @@
 package no.nav.dagpenger.rapportering.personregister.mediator.db
 
+import kotliquery.Row
 import kotliquery.queryOf
 import kotliquery.sessionOf
 import kotliquery.using
@@ -14,103 +15,105 @@ import javax.sql.DataSource
 class PostgresPersonRepository(
     private val dataSource: DataSource,
 ) : PersonRepository {
-    override fun finn(ident: String): Person? =
-        using(sessionOf(dataSource)) { session ->
-            val hendelser =
-                session.run(
-                    queryOf(
-                        "SELECT * FROM hendelse WHERE ident = :ident",
-                        mapOf("ident" to ident),
-                    ).map { row ->
-                        Hendelse(
-                            id = row.uuid("id"),
-                            ident = row.string("ident"),
-                            referanseId = row.string("referanse_id"),
-                            dato = row.localDateTime("dato"),
-                            status = Status.valueOf(row.string("status")),
-                            kilde = Kildesystem.valueOf(row.string("kilde")),
-                        )
-                    }.asList,
-                )
+    override fun finn(ident: String): Person? {
+        val hendelser = hentHendelser(ident)
+        val statusHistorikk = hentStatusHistorikk(ident).allItems()
 
-            if (hendelser.isNotEmpty()) {
-                Person(ident).apply {
-                    hendelser.forEach { hendelse ->
-                        this.hendelser.add(hendelse)
-                    }
-
-                    hentStatusHistorikk(dataSource, ident).allItems().forEach { (dato, status) ->
-                        this.statusHistorikk.put(dato, status)
-                    }
-                }
-            } else {
-                null
+        return if (hendelser.isNotEmpty()) {
+            Person(ident).apply {
+                hendelser.forEach { this.hendelser.add(it) }
+                statusHistorikk.forEach { (dato, status) -> this.statusHistorikk.put(dato, status) }
             }
+        } else {
+            null
         }
+    }
 
     override fun lagre(person: Person) {
         using(sessionOf(dataSource)) { session ->
             session.run(
-                queryOf(
-                    "INSERT INTO person (ident) VALUES (:ident)",
-                    mapOf("ident" to person.ident),
-                ).asUpdate,
+                queryOf("INSERT INTO person (ident) VALUES (:ident)", mapOf("ident" to person.ident)).asUpdate,
             )
         }
-        lagreStatus(dataSource, person.ident, LocalDateTime.now(), person.status)
-        person.hendelser.forEach { hendelse ->
-            using(sessionOf(dataSource)) { session ->
-                session.run(
-                    queryOf(
-                        "INSERT INTO hendelse (id, ident, referanse_id, dato, status, kilde) VALUES (?, ?, ?, ?, ?, ?)",
-                        hendelse.id,
-                        hendelse.ident,
-                        hendelse.referanseId,
-                        hendelse.dato,
-                        hendelse.status.name,
-                        hendelse.kilde.name,
-                    ).asUpdate,
-                )
+
+        person.hendelser.forEach { lagreHendelse(it) }
+        person.statusHistorikk
+            .allItems()
+            .forEach { (dato, status) ->
+                lagreStatusHistorikk(person.ident, dato, status)
             }
-        }
     }
 
     override fun oppdater(person: Person) {
         TODO("Not yet implemented")
     }
-}
 
-fun lagreStatus(
-    dataSource: DataSource,
-    personIdent: String,
-    dato: LocalDateTime,
-    status: Status,
-) {
-    using(sessionOf(dataSource)) { session ->
-        session.run(
-            queryOf(
-                "INSERT INTO status_historikk (person_ident, dato, status) VALUES (:person_ident, :dato, :status)",
-                mapOf("person_ident" to personIdent, "dato" to dato, "status" to status.toString()),
-            ).asUpdate,
-        )
+    private fun lagreHendelse(hendelse: Hendelse) {
+        using(sessionOf(dataSource)) { session ->
+            session.run(
+                queryOf(
+                    """
+                    INSERT INTO hendelse (id, ident, referanse_id, dato, status, kilde) 
+                    VALUES (?, ?, ?, ?, ?, ?)
+                    """,
+                    hendelse.id,
+                    hendelse.ident,
+                    hendelse.referanseId,
+                    hendelse.dato,
+                    hendelse.status.name,
+                    hendelse.kilde.name,
+                ).asUpdate,
+            )
+        }
     }
-}
 
-fun hentStatusHistorikk(
-    dataSource: DataSource,
-    personIdent: String,
-): TemporalCollection<Status> =
-    using(sessionOf(dataSource)) { session ->
-        val statusHistorikk = TemporalCollection<Status>()
-        session.run(
-            queryOf(
-                "SELECT * FROM status_historikk WHERE person_ident = :person_ident",
-                mapOf("person_ident" to personIdent),
-            ).map { row ->
-                val dato = row.localDateTime("dato")
-                val status = Status.valueOf(row.string("status"))
-                statusHistorikk.put(dato, status)
-            }.asList,
+    private fun hentHendelser(ident: String): List<Hendelse> =
+        using(sessionOf(dataSource)) { session ->
+            session.run(
+                queryOf("SELECT * FROM hendelse WHERE ident = :ident", mapOf("ident" to ident))
+                    .map { row -> tilHendelse(row) }
+                    .asList,
+            )
+        }
+
+    private fun tilHendelse(row: Row) =
+        Hendelse(
+            id = row.uuid("id"),
+            ident = row.string("ident"),
+            referanseId = row.string("referanse_id"),
+            dato = row.localDateTime("dato"),
+            status = Status.valueOf(row.string("status")),
+            kilde = Kildesystem.valueOf(row.string("kilde")),
         )
-        statusHistorikk
+
+    private fun lagreStatusHistorikk(
+        personIdent: String,
+        dato: LocalDateTime,
+        status: Status,
+    ) {
+        using(sessionOf(dataSource)) { session ->
+            session.run(
+                queryOf(
+                    "INSERT INTO status_historikk (person_ident, dato, status) VALUES (:person_ident, :dato, :status)",
+                    mapOf("person_ident" to personIdent, "dato" to dato, "status" to status.toString()),
+                ).asUpdate,
+            )
+        }
     }
+
+    private fun hentStatusHistorikk(personIdent: String): TemporalCollection<Status> =
+        TemporalCollection<Status>().apply {
+            using(sessionOf(dataSource)) { session ->
+                session.run(
+                    queryOf(
+                        "SELECT * FROM status_historikk WHERE person_ident = :person_ident",
+                        mapOf("person_ident" to personIdent),
+                    ).map { row ->
+                        val dato = row.localDateTime("dato")
+                        val status = Status.valueOf(row.string("status"))
+                        put(dato, status)
+                    }.asList,
+                )
+            }
+        }
+}
