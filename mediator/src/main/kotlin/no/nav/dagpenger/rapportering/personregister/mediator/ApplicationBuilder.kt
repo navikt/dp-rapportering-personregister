ee66572a77bf38d9ffd086b931ed19e6e37f8aee
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2024-09-26 12:03:31 +0200

    Cleaner databasen

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/ApplicationBuilder.kt
@@ -4,6 +4,7 @@ import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import no.nav.dagpenger.rapportering.personregister.mediator.api.internalApi
 import no.nav.dagpenger.rapportering.personregister.mediator.api.konfigurasjon
 import no.nav.dagpenger.rapportering.personregister.mediator.api.personstatusApi
+import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.clean
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.dataSource
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresPersonRepository
@@ -37,6 +38,7 @@ internal class ApplicationBuilder(
     }
 
     override fun onStartup(rapidsConnection: RapidsConnection) {
+        clean()
         runMigration()
     }
 }
