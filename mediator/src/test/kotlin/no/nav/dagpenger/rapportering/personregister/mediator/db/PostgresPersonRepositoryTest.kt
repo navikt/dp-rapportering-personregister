992ac9eb228127251e2f1c6f578ae0d7f9bede3c
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2024-09-26 14:27:37 +0200

    Legger til test for uthenting av antall personer og hendelser lagret i databasen

--- mediator/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/db/PostgresPersonRepositoryTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/db/PostgresPersonRepositoryTest.kt
@@ -46,4 +46,28 @@ class PostgresPersonRepositoryTest {
             personRepository.hentPerson("12345678901") shouldBe null
         }
     }
+
+    @Test
+    fun `kan hente antall personsoner og hendelser i databasen`() {
+        withMigratedDb {
+            val ident = "12345678901"
+            val referanseId = "123"
+            val dato = LocalDateTime.now()
+            val person = Person(ident = ident)
+            val hendelse =
+                Hendelse(
+                    ident = ident,
+                    referanseId = referanseId,
+                    dato = dato,
+                    status = Status.Søkt,
+                    kilde = Kildesystem.Søknad,
+                )
+
+            person.behandle(hendelse)
+            personRepository.lagrePerson(person)
+
+            personRepository.hentAnallPersoner() shouldBe 1
+            personRepository.hentAntallHendelser() shouldBe 1
+        }
+    }
 }
