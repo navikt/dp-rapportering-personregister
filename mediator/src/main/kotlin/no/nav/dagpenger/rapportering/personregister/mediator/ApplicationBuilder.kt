8a652fbb5a7dd27374b4c199fa9a81b330d5267b
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2024-11-15 09:51:11 +0100

    Cleaner databasen, siden arbeidssøker-feltet i person-tabellen er fjernet

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/ApplicationBuilder.kt
@@ -10,6 +10,7 @@ import no.nav.dagpenger.rapportering.personregister.mediator.api.internalApi
 import no.nav.dagpenger.rapportering.personregister.mediator.api.konfigurasjon
 import no.nav.dagpenger.rapportering.personregister.mediator.api.personstatusApi
 import no.nav.dagpenger.rapportering.personregister.mediator.connector.ArbeidssøkerConnector
+import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.clean
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.dataSource
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresPersonRepository
@@ -61,6 +62,7 @@ internal class ApplicationBuilder(
     }
 
     override fun onStartup(rapidsConnection: RapidsConnection) {
+        clean()
         runMigration()
         databaseMetrikker.startRapporteringJobb(personRepository)
     }
