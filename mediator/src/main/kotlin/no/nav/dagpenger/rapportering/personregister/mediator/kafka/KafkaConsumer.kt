2cba370d4e07e22285a2c6cb2d4089e003e1908a
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2024-11-20 12:40:18 +0100

    Bruker Duration for poll, så vi slipper å bruke en deprecated versjon av funksjonen

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/kafka/KafkaConsumer.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/kafka/KafkaConsumer.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.rapportering.personregister.mediator.kafka
 import com.github.navikt.tbd_libs.kafka.AivenConfig
 import com.github.navikt.tbd_libs.kafka.ConsumerProducerFactory
 import mu.KotlinLogging
+import java.time.Duration
 
 class KafkaConsumer {
     val factory = ConsumerProducerFactory(AivenConfig.default)
@@ -14,7 +15,7 @@ class KafkaConsumer {
                 logger.info { "Consumer subscribed to topic 'my-topic'" }
 
                 while (true) {
-                    val records = consumer.poll(1000) // Poll messages every second
+                    val records = consumer.poll(Duration.ofMillis(1000)) // Poll messages every second
                     records.forEach { record ->
                         logger.info {
                             "Received message: Key=${record.key()}, Value=${record.value()}"
