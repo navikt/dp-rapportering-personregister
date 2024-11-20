442dd006cfc9d199948b58a8472bcc6f11ccf709
abdifatah-bashi <abdifatah.bashi@nav.no>
2024-11-20 10:02:07 +0100

    Setter oppp test-endepunkt for ren Kafka
    Co-authored-by: Yrjan Fraschetti <yrjan.fraschetti@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/api/InternalApi.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/api/InternalApi.kt
@@ -1,12 +1,25 @@
 package no.nav.dagpenger.rapportering.personregister.mediator.api
 
+import com.github.navikt.tbd_libs.kafka.AivenConfig
+import com.github.navikt.tbd_libs.kafka.ConsumerProducerFactory
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import mu.KotlinLogging
+import org.apache.kafka.clients.producer.ProducerRecord
+
+data class KafkaMessage(
+    val key: String,
+    val value: String,
+)
+
+private val logger = KotlinLogging.logger {}
 
 fun Application.internalApi(meterRegistry: PrometheusMeterRegistry) {
     routing {
@@ -21,6 +34,20 @@ fun Application.internalApi(meterRegistry: PrometheusMeterRegistry) {
         }
         get("/metrics") {
             call.respondText(meterRegistry.scrape())
+        }
+
+        post("/test") {
+            try {
+                val body = call.receive<KafkaMessage>()
+
+                val factory = ConsumerProducerFactory(AivenConfig.default)
+                val produsent = factory.createProducer()
+
+                produsent.send(ProducerRecord("my-topic", body.key, body.value))
+            } catch (e: Exception) {
+                logger.error(e) { "Failed to process message" }
+                call.respond(HttpStatusCode.InternalServerError)
+            }
         }
     }
 }
