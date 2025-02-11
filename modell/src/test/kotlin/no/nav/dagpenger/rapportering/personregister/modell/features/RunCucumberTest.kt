e908564e8c5792b27c03e4d3bbd1a3934f46b8c5
abdifatah-bashi <abdifatah.bashi@nav.no>
2025-02-11 14:24:01 +0100

    Skriv cucumber-test for søknad-innvilget-stanset flyt

--- modell/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/modell/features/RunCucumberTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/modell/features/RunCucumberTest.kt
@@ -1,11 +1,13 @@
 package no.nav.dagpenger.rapportering.personregister.modell.features
 
+import io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME
 import io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 
 @Suite
-@SelectClasspathResource("features") // This points to src/test/resources/features
+@SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "no.nav.dagpenger.rapportering.personregister.modell.features.steps")
 class RunCucumberTest
