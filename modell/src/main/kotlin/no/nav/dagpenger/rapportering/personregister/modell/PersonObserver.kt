5869d737fd0658479011ff38202548944ec5b00b
abdifatah-bashi <abdifatah.bashi@nav.no>
2025-02-11 12:31:11 +0100

    Refaktorer og fjern ubrukt kode

---
+++ modell/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/modell/PersonObserver.kt
@@ -0,0 +1,7 @@
+package no.nav.dagpenger.rapportering.personregister.modell
+
+interface PersonObserver {
+    fun overtaArbeidssøkerBekreftelse(person: Person) {}
+
+    fun frasiArbeidssøkerBekreftelse(person: Person)
+}
