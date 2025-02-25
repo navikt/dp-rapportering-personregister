72525b554464404734981f71b9db7946226c8c8d
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2025-02-25 10:12:29 +0100

    Fjerner clean

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/ApplicationBuilder.kt
@@ -15,7 +15,6 @@ import no.nav.dagpenger.rapportering.personregister.mediator.Configuration.kafka
 import no.nav.dagpenger.rapportering.personregister.mediator.api.internalApi
 import no.nav.dagpenger.rapportering.personregister.mediator.api.personstatusApi
 import no.nav.dagpenger.rapportering.personregister.mediator.connector.ArbeidssøkerConnector
-import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.clean
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.dataSource
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.rapportering.personregister.mediator.db.PostgresPersonRepository
@@ -114,7 +113,6 @@ internal class ApplicationBuilder(
     }
 
     override fun onStartup(rapidsConnection: RapidsConnection) {
-        clean()
         runMigration()
         databaseMetrikker.startRapporteringJobb(personRepository)
     }
