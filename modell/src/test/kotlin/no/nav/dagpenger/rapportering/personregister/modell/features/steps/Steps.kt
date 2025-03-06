3f91a086c85c6072dcd78a8f887de4ac2f463a24
abdifatah-bashi <abdifatah.bashi@nav.no>
2025-03-06 11:13:16 +0100

    Skriv test for stans-klage flyt

--- modell/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/modell/features/steps/Steps.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/modell/features/steps/Steps.kt
@@ -15,10 +15,7 @@ import java.util.UUID
 class Steps : No {
     private lateinit var person: Person
     private val ident = "12345678901"
-    private val id = UUID.randomUUID().toString()
-    private val periodeId = UUID.randomUUID()
     private val nå = LocalDateTime.now()
-    private val tidligere = nå.minusDays(1)
 
     init {
         Gitt("en arbeidssøker") {
@@ -52,7 +49,7 @@ class Steps : No {
                 .apply { behandle(dagpengerMeldegruppeHendelse()) }
         }
 
-        Når("personen senere får innvilget dagpenger") {
+        Når("personen får innvilget dagpenger") {
             person.behandle(dagpengerMeldegruppeHendelse())
         }
 
@@ -63,12 +60,15 @@ class Steps : No {
         Når("personen klager på vedtaket") {
             person.behandle(dagpengerMeldegruppeHendelse())
         }
+
         Når("personen får avslag") {
             person.behandle(annenMeldegruppeHendelse())
         }
+
         Når("personen får medhold i klagen") {
             person.behandle(dagpengerMeldegruppeHendelse())
         }
+
         Når("personen får avslag på klagen") {
             person.behandle(annenMeldegruppeHendelse())
         }
@@ -88,86 +88,6 @@ class Steps : No {
         Og("vi frasier oss ansvaret for personen") {
             person.arbeidssøkerperioder.gjeldende?.overtattBekreftelse shouldBe false
         }
-
-        /*init {
-        Gitt("en person") {
-            person = Person(ident)
-        }
-
-        Gitt("en person som søkt om dagpenger den {string} med søknadId {string}") { søknadsdato: String, søknadId: String ->
-            person = Person(ident)
-            person.behandle(SøknadHendelse(ident, LocalDateTime.parse(søknadsdato), søknadId))
-        }
-
-        Gitt("en person som fikk stans fra {string}") { stansDato: String ->
-            person = Person(ident)
-            person.behandle(AnnenMeldegruppeHendelse(ident, LocalDateTime.parse(stansDato), "ARBS", "123"))
-        }
-
-        Når("personen søker om dagpenger den {string} med søknadId {string}") { søknadsdato: String, søknadId: String ->
-            person.behandle(SøknadHendelse(ident, LocalDateTime.parse(søknadsdato), søknadId))
-        }
-
-        Når("personen får vedtak om stans den {string} med vedtakId {string}") { vedtaksdato: String, vedtakId: String ->
-            person.behandle(AnnenMeldegruppeHendelse(ident, LocalDateTime.parse(vedtaksdato), "ARBS", vedtakId))
-        }
-
-        Når("personen senere får innvilget dagpenger") {
-            person.behandle(DagpengerMeldegruppeHendelse(ident, LocalDateTime.now(), "DAGP", UUID.randomUUID().toString()))
-        }
-
-        Når("personen senere får avslag") {
-            person.behandle(AnnenMeldegruppeHendelse(ident, LocalDateTime.now(), "ARBS", UUID.randomUUID().toString()))
-        }
-
-        Når("personen klager på vedtaket") {
-            person.behandle(DagpengerMeldegruppeHendelse(ident, LocalDateTime.now(), "DAGP", UUID.randomUUID().toString()))
-        }
-        Når("personen senere får medhold i klagen") {
-            person.behandle(DagpengerMeldegruppeHendelse(ident, LocalDateTime.now(), "DAGP", UUID.randomUUID().toString()))
-        }
-        Når("personen senere får avslag på klagen") {
-            person.behandle(AnnenMeldegruppeHendelse(ident, LocalDateTime.now(), "ARBS", UUID.randomUUID().toString()))
-        }
-
-        Når("personen registrerer seg som arbeidssøker og søker dagpenger") {
-            SøknadHendelse(
-                ident,
-                LocalDateTime.now(),
-                UUID.randomUUID().toString(),
-            ).apply { person.behandle(this) }
-
-            StartetArbeidssøkerperiodeHendelse(
-                UUID.randomUUID(),
-                ident,
-                LocalDateTime.now(),
-            ).apply { håndter(person) }
-        }
-
-        Når("vedtaket til personen blir stanset") {
-            person.behandle(AnnenMeldegruppeHendelse(ident, LocalDateTime.now(), "ARBS", UUID.randomUUID().toString()))
-        }
-
-        Så("skal personen være {string}") { status: String ->
-
-            person.status.type shouldBe Status.Type.valueOf(status)
-        }
-        Og("vi skal ikke lenger ha ansvaret for personen") {
-            person.arbeidssøkerperioder.first().overtattBekreftelse shouldBe false
-        }
-
-        Og("vi skal ha overtatt bekreftelse for personen") {
-            person.arbeidssøkerperioder.first().overtattBekreftelse shouldBe true
-        }
-
-        Og("vi skal fortsatt ha ansvaret for personen") {
-            person.arbeidssøkerperioder.first().overtattBekreftelse shouldBe true
-        }
-
-        Og("vi skal ikke lenger ha ansvaret for arbeidssøkeren") {
-            person.arbeidssøkerperioder.first().overtattBekreftelse shouldBe false
-        }
-    }*/
     }
 
     private fun dagpengerMeldegruppeHendelse(
--- modell/src/test/resources/features/person.innvilgelse.stans.feature
+++ modell/src/test/resources/features/person.innvilgelse.stans.feature
@@ -2,7 +2,7 @@
 Egenskap: Person
   Brukere som registrerer seg som arbeidssøker og søker dagpenger
 
-  Scenario: Søknad, innvilgelse og stans av dagpenger.
+  Scenario: Stans etter innvilgelse av dagpenger
     Gitt en arbeidssøker
     Når personen søker dagpenger
     Så skal personen være "IKKE_DAGPENGERBRUKER"
@@ -10,11 +10,60 @@ Egenskap: Person
     Så skal personen være "DAGPENGERBRUKER"
     Og vi har overtatt bekreftelsen for personen
 
-    Når personen senere får innvilget dagpenger
+    Når personen får innvilget dagpenger
     Så skal personen være "DAGPENGERBRUKER"
     Og vi beholder ansvaret for arbeidssøkerbekreftelse
 
     Når vedtaket til personen blir stanset
     Så skal personen være "IKKE_DAGPENGERBRUKER"
     Og vi frasier oss ansvaret for personen
+
+
+  Scenario: Klage på stans fører til medhold
+    Gitt en arbeidssøker
+    Når personen søker dagpenger
+    Når personen får meldeplikt og DAGP-meldegruppe
+    Så skal personen være "DAGPENGERBRUKER"
+    Og vi har overtatt bekreftelsen for personen
+
+    Når personen får innvilget dagpenger
+    Så skal personen være "DAGPENGERBRUKER"
+    Og vi beholder ansvaret for arbeidssøkerbekreftelse
+
+    Når vedtaket til personen blir stanset
+    Så skal personen være "IKKE_DAGPENGERBRUKER"
+    Og vi frasier oss ansvaret for personen
+
+    Når personen klager på vedtaket
+    Så skal personen være "DAGPENGERBRUKER"
+    Og vi har overtatt bekreftelsen for personen
+
+    Når personen får medhold i klagen
+    Så skal personen være "DAGPENGERBRUKER"
+    Og vi beholder ansvaret for arbeidssøkerbekreftelse
+
+  Scenario: Klage på stans fører ikke til medhold
+    Gitt en arbeidssøker
+    Når personen søker dagpenger
+    Når personen får meldeplikt og DAGP-meldegruppe
+    Så skal personen være "DAGPENGERBRUKER"
+    Og vi har overtatt bekreftelsen for personen
+
+    Når personen får innvilget dagpenger
+    Så skal personen være "DAGPENGERBRUKER"
+    Og vi beholder ansvaret for arbeidssøkerbekreftelse
+
+    Når vedtaket til personen blir stanset
+    Så skal personen være "IKKE_DAGPENGERBRUKER"
+    Og vi frasier oss ansvaret for personen
+
+    Når personen klager på vedtaket
+    Så skal personen være "DAGPENGERBRUKER"
+    Og vi har overtatt bekreftelsen for personen
+
+    Når personen får avslag på klagen
+    Så skal personen være "IKKE_DAGPENGERBRUKER"
+    Og vi frasier oss ansvaret for personen
+
+
 
