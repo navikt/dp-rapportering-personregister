1733bded0e6a08b72e52ab3a202b6ecb6e7fea71
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2025-03-18 09:27:41 +0100

    Oppdaterer avro schemas for arbeidssøkerregister-modeller

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     implementation(libs.rapids.and.rivers)
     implementation(libs.konfig)
     implementation(libs.kotlin.logging)
-    paavegneavSchema("no.nav.paw.arbeidssokerregisteret.api:bekreftelse-paavegneav-schema:25.02.07.15-1")
-    mainavroSchema("no.nav.paw.arbeidssokerregisteret.api:main-avro-schema:1.13764081353.1-2")
+    paavegneavSchema("no.nav.paw.arbeidssokerregisteret.api:bekreftelse-paavegneav-schema:1.25.03.11.31-1")
+    mainavroSchema("no.nav.paw.arbeidssokerregisteret.api:main-avro-schema:1.25.03.10.2-1")
 
     testImplementation(platform("org.junit:junit-bom:5.12.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
