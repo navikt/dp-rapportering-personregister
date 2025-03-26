f4011e3a58363d073736449dab45c3f82018b658
abdifatah-bashi <abdifatah.bashi@nav.no>
2025-03-26 14:24:51 +0100

    Fjern duplikate statushistorikk

--- mediator/src/main/resources/db/migration/V6__STATUS_HISTORIKK_CONSTRAINT.sql
+++ mediator/src/main/resources/db/migration/V6__STATUS_HISTORIKK_CONSTRAINT.sql
@@ -1,2 +1,9 @@
+DELETE FROM status_historikk a
+    USING status_historikk b
+WHERE a.ctid < b.ctid
+  AND a.person_id = b.person_id
+  AND a.dato = b.dato;
+
+
 ALTER TABLE status_historikk
 ADD CONSTRAINT unique_person_dato UNIQUE (person_id, dato);
