5bce72148b80a209af587b3250ddf2cc9495e5a8
Igor Weber <igor.shuliakov@nav.no>
2025-11-05 13:16:35 +0100

    SøknadMottak prosesserer innsending_ferdigstilt (ikke søknad_innsendt_varsel)

--- mediator/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/tjenester/SøknadMottakTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/tjenester/SøknadMottakTest.kt
@@ -8,7 +8,8 @@ import no.nav.dagpenger.rapportering.personregister.mediator.utils.MetrikkerTest
 import no.nav.dagpenger.rapportering.personregister.modell.hendelser.SøknadHendelse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.time.OffsetDateTime
+import java.time.LocalDateTime
+import java.util.UUID
 
 class SøknadMottakTest {
     private val testRapid = TestRapid()
@@ -24,12 +25,13 @@ class SøknadMottakTest {
     }
 
     @Test
-    fun `skal motta søknad innsendt event`() {
-        val ident = "12345678903"
-        val søknadId = "123e4567-e89b-12d3-a456-426614174000"
-        val dato = "2024-10-03T16:14:40+02:00"
+    fun `skal motta Quiz-søknad`() {
+        val ident = "12345678901"
+        val søknadId = UUID.randomUUID().toString()
+        val søknadsData = "\"søknad_uuid\": \"$søknadId\""
+        val dato = "2025-09-23T00:00:00"
 
-        testRapid.sendTestMessage(lagSøknadInnsendtEvent(ident, dato, søknadId))
+        testRapid.sendTestMessage(lagInnsendingFerdigstiltEvent(ident, dato, søknadsData))
 
         val søknadHendelse =
             SøknadHendelse(
@@ -41,23 +43,74 @@ class SøknadMottakTest {
 
         verify(exactly = 1) { personMediator.behandle(søknadHendelse) }
     }
+
+    @Test
+    fun `skal motta Legacy-søknad`() {
+        val ident = "12345678902"
+        val søknadId = UUID.randomUUID().toString()
+        val søknadsData = "\"brukerBehandlingId\": \"$søknadId\""
+        val dato = "2025-09-24T00:00:00"
+
+        testRapid.sendTestMessage(lagInnsendingFerdigstiltEvent(ident, dato, søknadsData))
+
+        val søknadHendelse =
+            SøknadHendelse(
+                ident,
+                dato.toLocalDateTime(),
+                dato.toLocalDateTime(),
+                søknadId,
+            )
+
+        verify(exactly = 1) { personMediator.behandle(søknadHendelse) }
+    }
+
+    @Test
+    fun `skal motta Papirsøknad`() {
+        val ident = "12345678903"
+        val søknadsData = null
+        val dato = "2025-09-25T00:00:00"
+
+        testRapid.sendTestMessage(lagInnsendingFerdigstiltEvent(ident, dato, søknadsData))
+
+        verify(exactly = 1) { personMediator.behandle(any<SøknadHendelse>(), eq(1)) }
+    }
 }
 
-private fun lagSøknadInnsendtEvent(
+private fun lagInnsendingFerdigstiltEvent(
     ident: String,
     dato: String,
-    søknadId: String,
+    søknadsData: String?,
 ): String {
     //language=json
     return """
         {
-          "@event_name": "søknad_innsendt_varsel",
-          "ident": "$ident",
-          "søknadId": "$søknadId",
-          "søknadstidspunkt": "$dato" 
-          
+        	"@id": "7b4d1240-35ef-413f-b7e0-e0ef6e7677e9",
+        	"@opprettet": "2025-09-25T17:26:30.406457503",
+        	"journalpostId": "721541000",
+        	"datoRegistrert": "$dato",
+        	"skjemaKode": "NAV 04-01.04",
+        	"tittel": "NAV 04-01.04 Søknad om dagpenger ved permittering",
+        	"type": "NySøknad",
+        	"fødselsnummer": "$ident",
+        	"aktørId": "1000012345678",
+        	"fagsakId": "null",
+        	"søknadsData": {
+              ${søknadsData ?: ""}
+            },
+        	"@event_name": "innsending_ferdigstilt",
+        	"bruk-dp-behandling": true,
+        	"system_read_count": 0,
+        	"system_participating_services": [
+        		{
+        			"id": "7b4d1240-35ef-413f-b7e0-e0ef6e7677e9",
+        			"time": "2025-09-25T17:26:30.406621723",
+        			"service": "dp-mottak",
+        			"instance": "dp-mottak-77c76f7895-tqlq6",
+        			"image": "europe-north1-docker.pkg.dev/nais-management-233d/teamdagpenger/dp-mottak:2025.09.24-06.59-661ca55"
+        		}
+        	]
         }
         """.trimIndent()
 }
 
-private fun String.toLocalDateTime() = OffsetDateTime.parse(this).toLocalDateTime()
+private fun String.toLocalDateTime() = LocalDateTime.parse(this)
