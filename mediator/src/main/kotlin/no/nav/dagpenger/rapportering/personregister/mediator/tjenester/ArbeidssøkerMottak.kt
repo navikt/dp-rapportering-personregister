527e450020968edb4dc9e651db24281e76d1440a
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2026-01-27 13:41:33 +0100

    Logger avsluttet-metadata ved mottak av arbeidssøkerperioder

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/tjenester/ArbeidssøkerMottak.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/tjenester/ArbeidssøkerMottak.kt
@@ -18,6 +18,14 @@ class ArbeidssøkerMottak(
     fun consume(records: ConsumerRecords<Long, Periode>) =
         records.forEach { record ->
             logger.info { "Behandler periode med key: ${record.key()}" }
+
+            teamLogg.info {
+                "Behandler periode med key: ${record.key()}. " +
+                    "Avsluttet.kilde: ${record.value().avsluttet?.kilde}. " +
+                    "Avsluttet.aarsak: ${record.value().avsluttet?.aarsak}. " +
+                    "Avsluttet.utfoertAv.id: ${record.value().avsluttet?.utfoertAv?.id}. " +
+                    "Avsluttet.utfoertAv.id: ${record.value().avsluttet?.utfoertAv?.type?.name}"
+            }
             arbeidssøkerperiodeMetrikker.arbeidssøkerperiodeMottatt.increment()
             Arbeidssøkerperiode(
                 ident = record.value().identitetsnummer,
@@ -30,5 +38,6 @@ class ArbeidssøkerMottak(
 
     companion object {
         private val logger = KotlinLogging.logger {}
+        private val teamLogg = KotlinLogging.logger("tjenestekall")
     }
 }
