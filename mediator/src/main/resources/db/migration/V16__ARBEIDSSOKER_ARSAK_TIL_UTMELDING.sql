2ea74693934304d4d3b85b4ca868645f704e2736
abdifatah-bashi <abdifatah.bashi@nav.no>
2026-04-20 13:16:19 +0200

    Utvid arbeidssøker tabell med nytt felt: arsak_til_utmelding
    Co-authored-by: Marianne Grov <marianne.grov@nav.no>

---
+++ mediator/src/main/resources/db/migration/V16__ARBEIDSSOKER_ARSAK_TIL_UTMELDING.sql
@@ -0,0 +1,4 @@
+ALTER TABLE arbeidssoker ADD COLUMN  aarsak_til_utmelding VARCHAR(50) NULL default NULL;
+
+
+
--- modell/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/modell/Arbeidssøkerperiode.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/rapportering/personregister/modell/Arbeidssøkerperiode.kt
@@ -9,9 +9,16 @@ data class Arbeidssøkerperiode(
     val startet: LocalDateTime,
     var avsluttet: LocalDateTime?,
     var overtattBekreftelse: Boolean?,
+    var årsakTilUtmelding: ÅrsakTilUtmelding? = null,
 )
 
 fun Arbeidssøkerperiode.aktiv(): Boolean = avsluttet == null
 
 val List<Arbeidssøkerperiode>.gjeldende: Arbeidssøkerperiode?
     get() = this.firstOrNull { it.aktiv() }
+
+enum class ÅrsakTilUtmelding {
+    UTMELDT_I_ASR,
+    MELDEPLIKT_BRUTT,
+    UTMELDT_PÅ_MELDEKORT,
+}
