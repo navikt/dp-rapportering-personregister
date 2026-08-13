ef2ce06bfbdb317efe46a22c08889140e071605d
Vegard Skjefstad <660277+vskjefst@users.noreply.github.com>
2026-08-13 09:23:58 +0200

    Legg til tidspunkt for når innkommende og utgående melding blir lagret (#590)

---
+++ mediator/src/main/resources/db/migration/V20__.LEGG_TIL_KOLONNE_TIDSPUNKT_I_MELDING_TABELLER.sql
@@ -0,0 +1,2 @@
+ALTER TABLE meldinger_innkommende ADD COLUMN tidspunkt TIMESTAMP;
+ALTER TABLE meldinger_utgående ADD COLUMN tidspunkt TIMESTAMP;
--- mediator/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/db/MeldingerRepositoryPostgresTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/rapportering/personregister/mediator/db/MeldingerRepositoryPostgresTest.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.rapportering.personregister.mediator.db
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.rapportering.personregister.mediator.db.Postgres.dataSource
 import no.nav.dagpenger.rapportering.personregister.mediator.db.Postgres.withMigratedDb
 import no.nav.dagpenger.rapportering.personregister.mediator.utils.UUIDv7
@@ -10,56 +11,68 @@ class MeldingerRepositoryPostgresTest {
     private val meldingerRepository = MeldingerRepositoryPostgres(dataSource)
 
     @Test
-    fun `kan lagre innkommende melding`() =
+    fun `lagreInnkommendeMelding kan lagre innkommende melding`() =
         withMigratedDb {
             val ident = "12345678901"
 
-            shouldNotThrowAny {
-                meldingerRepository.lagreInnkommendeMelding(
-                    ident = ident,
-                    relevantMeldingsinnhold = "{ \"key\": \"value\" }",
-                )
-            }
+            val antallOpprettedeRader =
+                shouldNotThrowAny {
+                    meldingerRepository.lagreInnkommendeMelding(
+                        ident = ident,
+                        relevantMeldingsinnhold = "{ \"key\": \"value\" }",
+                    )
+                }
+
+            antallOpprettedeRader shouldBe 1
         }
 
     @Test
-    fun `kaster ikke exception hvis relevantMeldingsinnhold ikke er gyldig JSON`() =
+    fun `lagreInnkommendeMelding oppretter ingen rader og kaster ikke exception hvis relevantMeldingsinnhold ikke er gyldig JSON`() =
         withMigratedDb {
             val ident = "12345678901"
 
-            shouldNotThrowAny {
-                meldingerRepository.lagreInnkommendeMelding(
-                    ident = ident,
-                    relevantMeldingsinnhold = "bla bla bla",
-                )
-            }
+            val antallOpprettedeRader =
+                shouldNotThrowAny {
+                    meldingerRepository.lagreInnkommendeMelding(
+                        ident = ident,
+                        relevantMeldingsinnhold = "bla bla bla",
+                    )
+                }
+
+            antallOpprettedeRader shouldBe 0
         }
 
     @Test
-    fun `kan lagre utgående melding`() =
+    fun `lagreUtgåendeMelding kan lagre utgående melding`() =
         withMigratedDb {
             val ident = "12345678901"
 
-            shouldNotThrowAny {
-                meldingerRepository.lagreUtgåendeMelding(
-                    korrelasjonsId = UUIDv7.newUuid(),
-                    ident = ident,
-                    melding = "{ \"key\": \"value\" }",
-                )
-            }
+            val antallOpprettedeRader =
+                shouldNotThrowAny {
+                    meldingerRepository.lagreUtgåendeMelding(
+                        korrelasjonsId = UUIDv7.newUuid(),
+                        ident = ident,
+                        melding = "{ \"key\": \"value\" }",
+                    )
+                }
+
+            antallOpprettedeRader shouldBe 1
         }
 
     @Test
-    fun `kaster ikke exception hvis melding ikke er gyldig JSON`() =
+    fun `lagreUtgåendeMelding oppretter ingen rader og kaster ikke exception hvis melding ikke er gyldig JSON`() =
         withMigratedDb {
             val ident = "12345678901"
 
-            shouldNotThrowAny {
-                meldingerRepository.lagreUtgåendeMelding(
-                    korrelasjonsId = UUIDv7.newUuid(),
-                    ident = ident,
-                    melding = "bla bla bla",
-                )
-            }
+            val antallOpprettedeRader =
+                shouldNotThrowAny {
+                    meldingerRepository.lagreUtgåendeMelding(
+                        korrelasjonsId = UUIDv7.newUuid(),
+                        ident = ident,
+                        melding = "bla bla bla",
+                    )
+                }
+
+            antallOpprettedeRader shouldBe 0
         }
 }
